aa4c997df3b43db815ef9248e4786ae57829f479
Carl Youngblood <carl@blockscale.net>
2023-01-31 19:48:18 -0700

    Update sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,12 +5,12 @@ export function SideBar() {
     <h3>Sidebar</h3>
     <p className="pt-8 text text-sm">
       Copyright ©️ 2010-{new Date().getFullYear()} Carl Youngblood<br />
-      Shared under the <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0</a> license.<br />
+      Shared under the <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons BY-NC-ND 4.0</a> license.<br />
       <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">
         <Image
           className="w-24" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-nc-nd.svg"
           width="88" height="31"
-          alt="CC BY-NC-ND 4.0" />
+          alt="Creative Commons logo BY-NC-ND 4.0" />
       </a>
     </p>
   </div>;
